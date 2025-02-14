7ceb36d84f27aef1e6f975cfe2e94cef791a838b
TemDPCTicxar <jhonatan.rodriguez@ticxar.com>
2025-02-14 09:06:18 -0500

    subir proyecto

---
+++ gestionDocumenatalApp/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,ts}", // Importante para detectar los archivos de Angular
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
